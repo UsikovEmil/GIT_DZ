18d309bda7cc516ae9b764840ce45652b660cfcc
Emil <usikov.emil@yandex.ru>
2023-07-13 21:42:52 +0500

    Revert "Revert "Revert "Изменение 2"""
    This reverts commit 25a884bb94ebc16cbeb89c72799252dc480196e9.

--- DZ01/main01.java
+++ DZ01/main01.java
@@ -7,10 +7,10 @@ public class main01 {
         }
 
 
+      
         public static void main(String[] args) {
           System.out.println(myFunction(3));
-          System.out.println(myFunction(3));
         }
-
+        
       }
       
